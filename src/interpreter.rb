6f2377dff37d2787c831cd615e3ce3c423fb8d9d
Pascal Hurni <phi@pragmaticpurist.org>
2025-05-15 01:38:15 +0200

    Add source location reporting for interpreter errors

--- src/interpreter.rb
+++ src/interpreter.rb
@@ -17,7 +17,7 @@ class Interpreter
       node.items.each {|item| run(item, context) }
 
     when FunctionDefinition
-      raise RuntimeError.new("Trying to redefine function: #{node.name}") if @functions.has_key?(node.name)
+      raise RuntimeError.new(node.source_location, "Trying to redefine function: #{node.name}") if @functions.has_key?(node.name)
       @functions[node.name] = node
 
     when Statements
@@ -25,8 +25,8 @@ class Interpreter
 
     when FunctionCall
       function_node = @functions[node.name]
-      raise RuntimeError.new("Trying to call unknown function #{node.name}") unless function_node
-      raise RuntimeError.new("Calling function #{node.name} with #{node.actual_args.size} arguments when #{function_node.formal_args.size} needed") unless node.actual_args.size == function_node.formal_args.size
+      raise RuntimeError.new(node.source_location, "Trying to call unknown function #{node.name}") unless function_node
+      raise RuntimeError.new(node.source_location, "Calling function #{node.name} with #{node.actual_args.size} arguments when #{function_node.formal_args.size} needed") unless node.actual_args.size == function_node.formal_args.size
 
       new_context = function_node.formal_args.zip(node.actual_args.map {|arg| run(arg, context) }).to_h
       run(function_node.body, new_context)
@@ -47,11 +47,11 @@ class Interpreter
       context[node.name] = nil
 
     when VariableAssignment
-      raise RuntimeError.new("Trying to assign undeclared variable #{node.name}") unless context.has_key?(node.name)
+      raise RuntimeError.new(node.source_location, "Trying to assign undeclared variable #{node.name}") unless context.has_key?(node.name)
       context[node.name] = run(node.expression, context)
 
     when VariableReference
-      raise RuntimeError.new("Trying to reference undeclared variable #{node.name}") unless context.has_key?(node.name)
+      raise RuntimeError.new(node.source_location, "Trying to reference undeclared variable #{node.name}") unless context.has_key?(node.name)
       context[node.name]
 
     when IntegerLiteral
@@ -76,7 +76,7 @@ class Interpreter
       when '!=' then lhs == rhs
 
       else
-        raise RuntimeError.new("Unknown operator #{node.glyph}")
+        raise RuntimeError.new(node.source_location, "Unknown operator #{node.glyph}")
       end
 
     when Proc
