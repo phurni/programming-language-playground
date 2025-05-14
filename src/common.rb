1133e98bdda862429c9c48ab18a5ba2d020b464d
Pascal Hurni <phi@pragmaticpurist.org>
2025-05-15 01:09:25 +0200

    Prepare source location tracking for better error reporting

--- src/common.rb
+++ src/common.rb
@@ -1,2 +1,11 @@
+SourceLocation = Struct.new(:filename, :line_number) do
+  def to_s
+    "#{filename || '(none)'}:#{line_number}"
+  end
+end
+
 class CodeError < StandardError
+  def initialize(*captions)
+    super(captions.compact.join(': '))
+  end
 end
