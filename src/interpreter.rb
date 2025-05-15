28b8419591a634dcf048b479d4ed071e3e0e2a43
Pascal Hurni <phi@pragmaticpurist.org>
2025-05-15 17:59:14 +0200

    Refactor interpreter run method by removing definition nodes, re-indenting and propagating the last value for expressions

--- src/interpreter.rb
+++ src/interpreter.rb
@@ -50,79 +50,57 @@ class Interpreter
       node = next_node
       next_node = node.next
 
-    case node
-    when Definitions
-      node.items.each {|item| run(item, context) }
+      case node
+      when FunctionCall
+        function_node = @functions[node.name]
+        raise RuntimeError.new(node.source_location, "Trying to call unknown function #{node.name}") unless function_node
+        raise RuntimeError.new(node.source_location, "Calling function #{node.name} with #{node.actual_args.size} arguments when #{function_node.formal_args.size} needed") unless node.actual_args.size == function_node.formal_args.size
 
-    when FunctionDefinition
-      raise RuntimeError.new(node.source_location, "Trying to redefine function: #{node.name}") if @functions.has_key?(node.name)
-      @functions[node.name] = node
+        new_context = function_node.formal_args.zip(node.actual_args.map {|arg| run(arg, context) }).to_h
+        run(function_node.body, new_context)
 
-    when Statements
-      node.items.reduce(nil) {|last_value, item| run(item, context) }
+      when VariableDeclaration
+        context[node.name] = nil
 
-    when FunctionCall
-      function_node = @functions[node.name]
-      raise RuntimeError.new(node.source_location, "Trying to call unknown function #{node.name}") unless function_node
-      raise RuntimeError.new(node.source_location, "Calling function #{node.name} with #{node.actual_args.size} arguments when #{function_node.formal_args.size} needed") unless node.actual_args.size == function_node.formal_args.size
+      when VariableAssignment
+        raise RuntimeError.new(node.source_location, "Trying to assign undeclared variable #{node.name}") unless context.has_key?(node.name)
+        context[node.name] = run(node.expression, context)
 
-      new_context = function_node.formal_args.zip(node.actual_args.map {|arg| run(arg, context) }).to_h
-      run(function_node.body, new_context)
+      when VariableReference
+        raise RuntimeError.new(node.source_location, "Trying to reference undeclared variable #{node.name}") unless context.has_key?(node.name)
+        value = context[node.name]
 
-    when IfElse
-      if run(node.condition, context)
-        run(node.true_body, context)
-      else
-        run(node.false_body, context) if node.false_body
-      end
+      when IntegerLiteral
+        value = node.value
 
-    when While
-      while run(node.condition, context)
-        run(node.body, context)
-      end
+      when BinaryOperator
+        lhs = run(node.lhs, context)
+        rhs = run(node.rhs, context)
 
-    when VariableDeclaration
-      context[node.name] = nil
+        value = case node.glyph
+        when '*'  then lhs * rhs
+        when '/'  then lhs / rhs
+        when '%'  then lhs % rhs
+        when '+'  then lhs + rhs
+        when '-'  then lhs - rhs
 
-    when VariableAssignment
-      raise RuntimeError.new(node.source_location, "Trying to assign undeclared variable #{node.name}") unless context.has_key?(node.name)
-      context[node.name] = run(node.expression, context)
+        when '<'  then lhs < rhs
+        when '<=' then lhs <= rhs
+        when '>'  then lhs > rhs
+        when '>=' then lhs >= rhs
+        when '==' then lhs == rhs
+        when '!=' then lhs == rhs
 
-    when VariableReference
-      raise RuntimeError.new(node.source_location, "Trying to reference undeclared variable #{node.name}") unless context.has_key?(node.name)
-      context[node.name]
+        else
+          raise RuntimeError.new(node.source_location, "Unknown operator #{node.glyph}")
+        end
 
-    when IntegerLiteral
-      node.value
-
-    when BinaryOperator
-      lhs = run(node.lhs, context)
-      rhs = run(node.rhs, context)
-
-      case node.glyph
-      when '*'  then lhs * rhs
-      when '/'  then lhs / rhs
-      when '%'  then lhs % rhs
-      when '+'  then lhs + rhs
-      when '-'  then lhs - rhs
-
-      when '<'  then lhs < rhs
-      when '<=' then lhs <= rhs
-      when '>'  then lhs > rhs
-      when '>=' then lhs >= rhs
-      when '==' then lhs == rhs
-      when '!=' then lhs == rhs
+      when Proc
+        value = node.call(context)
 
       else
-        raise RuntimeError.new(node.source_location, "Unknown operator #{node.glyph}")
+        raise RuntimeError.new("Unexpected node type: #{node.class}")
       end
-
-    when Proc
-      node.call(context)
-
-    else
-      raise RuntimeError.new("Unexpected node type: #{node.class}")
-    end
     end
 
     value
